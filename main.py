c194078f2a5f8680f900f45c54766840b8657a25
nel215 <otomo.yuhei@gmail.com>
2018-05-22 23:11:41 +0900

    Add baseline lightgbm model

---
+++ main.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import lightgbm as lgb
+
+
+def train(df):
+    n_train = int(len(df) * 0.9)
+    train_df = df[:n_train]
+    valid_df = df[n_train:]
+    features = [
+        'EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3',
+    ]
+    xgtrain = lgb.Dataset(
+        train_df[features].values, label=train_df['TARGET'].values,
+        feature_name=features,
+    )
+    xgvalid = lgb.Dataset(
+        valid_df[features].values, label=valid_df['TARGET'].values,
+        feature_name=features,
+    )
+    evals_result = {}
+    lgb_params = {
+        'boosting_type': 'gbdt',
+        'objective': 'binary',
+        'metric': 'auc',
+        'learning_rate': 0.1,
+        'num_leaves': (2**3)-1,  # 2^max_depth - 1
+        'max_depth': 3,  # -1 means no limit
+        'min_child_samples': 100,
+        'max_bin': 100,
+        'subsample': 0.7,
+        'subsample_freq': 1,
+        'colsample_bytree': 0.9,
+        'min_child_weight': 5,
+        'subsample_for_bin': 200000,
+        'min_split_gain': 0,
+        'reg_alpha': 0,
+        'reg_lambda': 0,
+        # 'scale_pos_weight': 200,
+        'nthread': 12,
+        'verbose': 0,
+    }
+    bst = lgb.train(
+        lgb_params,
+        xgtrain,
+        valid_sets=[xgtrain, xgvalid],
+        valid_names=['train', 'valid'],
+        evals_result=evals_result,
+        num_boost_round=1000,
+        early_stopping_rounds=30,
+        verbose_eval=10,
+        # feval=feval,
+    )
+
+    print("\nModel Report")
+    print("bst1.best_iteration: ", bst.best_iteration)
+    print("auc:", evals_result['valid']['auc'][bst.best_iteration-1])
+
+
+def main():
+    train_df = pd.read_feather('./data/application_train.csv.feather')
+    train(train_df)
+
+
+if __name__ == '__main__':
+    main()

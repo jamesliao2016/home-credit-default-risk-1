ddce3605b03d4cf36d65d5952d9bc58b51e7ade9
nel215 <otomo.yuhei@gmail.com>
2018-06-04 21:40:06 +0900

    Add preprocessed features on credit

--- preprocess_credit.py
+++ preprocess_credit.py
@@ -1,5 +1,6 @@
 import pandas as pd
 pd.set_option("display.max_columns", 100)
+pd.set_option("display.width", 200)
 
 
 def handle_numeric(df, res, target):
@@ -37,6 +38,38 @@ def preprocess_credit(cred_df):
         ['SK_ID_CURR', 'SK_ID_PREV'])[['SK_DPD_PLUS']].sum()
     grp = grp.reset_index()
     res = handle_numeric(grp, res, 'SK_DPD_PLUS')
+
+    cred_df['SHORT_PAYMENT'] = (
+        cred_df['AMT_INST_MIN_REGULARITY'] > cred_df['AMT_PAYMENT_CURRENT']
+    ).astype('i')
+    grp = cred_df.groupby('SK_ID_CURR')['SHORT_PAYMENT'].sum()
+    grp = grp.reset_index()
+    res = res.merge(grp, on='SK_ID_CURR', how='left')
+    res['SHORT_PAYMENT_ratio'] = res['SHORT_PAYMENT'] / res['CREDIT_COUNT']
+
+    grp = cred_df.groupby('SK_ID_CURR')[[
+        'AMT_DRAWINGS_CURRENT',
+        'AMT_DRAWINGS_ATM_CURRENT',
+        'AMT_DRAWINGS_POS_CURRENT',
+        'AMT_DRAWINGS_OTHER_CURRENT',
+    ]].sum()
+    grp['AMT_DRAWINGS_ATM_CURRENT'] /= grp['AMT_DRAWINGS_CURRENT']
+    grp['AMT_DRAWINGS_POS_CURRENT'] /= grp['AMT_DRAWINGS_CURRENT']
+    grp['AMT_DRAWINGS_OTHER_CURRENT'] /= grp['AMT_DRAWINGS_CURRENT']
+    grp.pop('AMT_DRAWINGS_CURRENT')
+    grp.columns = ['{}_ratio'.format(c) for c in grp.columns]
+    grp = grp.reset_index()
+    res = res.merge(grp, on='SK_ID_CURR', how='left')
+
+    grp = cred_df.groupby('SK_ID_CURR')[[
+        'AMT_DRAWINGS_CURRENT',
+        'CNT_DRAWINGS_CURRENT',
+    ]].sum()
+    grp['AMT_DRAWINGS_CURRENT'] /= grp['CNT_DRAWINGS_CURRENT']
+    grp.pop('CNT_DRAWINGS_CURRENT')
+    grp.columns = ['MEAN_AMT_DRAWINGS_CURRENT']
+    grp = grp.reset_index()
+    res = res.merge(grp, on='SK_ID_CURR', how='left')
 
     return res
 

9ac87b3ed1a45033d29904fe2e2b7990d53a6426
nel215 <otomo.yuhei@gmail.com>
2018-06-05 20:57:02 +0900

    Refactor

--- encode_application_second_order_categorical.py
+++ encode_application_second_order_categorical.py
@@ -9,15 +9,14 @@ def main():
     test_df = pd.read_feather('./data/application_test.csv.feather')
 
     columns = train_df.select_dtypes('object').columns.tolist()
+    train_res = train_df[['SK_ID_CURR']].copy()
+    test_res = test_df[['SK_ID_CURR']].copy()
     train_df = train_df[columns + ['TARGET']]
     train_df.fillna('__NaN__', inplace=True)
     test_df = test_df[columns]
     test_df.fillna('__NaN__', inplace=True)
-    train_id = train_df.pop('SK_ID_CURR')
-    test_id = test_df.pop('SK_ID_CURR')
     target = train_df['TARGET']
-    train_res = train_df[[]].copy()
-    test_res = test_df[[]].copy()
+    encode_columns = []
     for i in range(len(columns)):
         a = columns[i]
         for j in range(i+1, len(columns)):
@@ -25,13 +24,12 @@ def main():
             c = 'ENCODED_{}__{}'.format(a, b)
             train_res[c] = train_df[a] + '--' + train_df[b]
             test_res[c] = test_df[a] + '--' + test_df[b]
+            encode_columns.append(c)
             gc.collect()
 
-    encoder = TargetEncoder()
+    encoder = TargetEncoder(cols=encode_columns)
     train_res = encoder.fit_transform(train_res, target)
-    train_res['SK_ID_CURR'] = train_id
     test_res = encoder.transform(test_res)
-    test_res['SK_ID_CURR'] = test_id
     train_res.to_feather(
         './data/application_train.2nd-order-categorical.feather')
     test_res.to_feather(

02ead6e9ac5edbd17ddeaba0fb7e08f59e33af61
nel215 <otomo.yuhei@gmail.com>
2018-06-05 22:25:28 +0900

    Refactor 2nd order numeric feature extraction

--- encode_inst.py
+++ encode_inst.py
@@ -1,28 +1,14 @@
-import numpy as np
 import pandas as pd
+from utility import calc_2nd_order_feature
 pd.set_option("display.max_columns", 100)
 
 
 def main():
     inst_df = pd.read_feather('./data/installments_payments.csv.feather')
-    columns = inst_df.select_dtypes([np.number]).columns.tolist()
-    columns.remove('SK_ID_CURR')
-    columns.remove('SK_ID_PREV')
-
-    grp = inst_df.groupby('SK_ID_CURR')[columns].sum().reset_index()
-    tcolumns = []
-    for i in range(len(columns)):
-        a = columns[i]
-        for j in range(i+1, len(columns)):
-            b = columns[j]
-            c = 'NUMERIC_DIV_{}__{}'.format(a, b)
-            d = 'NUMERIC_MUL_{}__{}'.format(a, b)
-            tcolumns += [c, d]
-            grp[c] = grp[a] / grp[b]
-            grp[d] = grp[a] * grp[b]
-
-    grp = grp[['SK_ID_CURR'] + tcolumns]
-    grp.to_feather('./data/installments_payments.numeric.2nd-order.feather')
+    res_df = calc_2nd_order_feature(inst_df).set_index('SK_ID_CURR')
+    res_df.columns = ['inst_{}'.format(c) for c in res_df.columns]
+    res_df = res_df.reset_index()
+    res_df.to_feather('./data/installments_payments.numeric.2nd-order.feather')
 
 
 if __name__ == '__main__':

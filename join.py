7b476d7bbe445795af41bd5e7c8507c2ed278d0b
nel215 <otomo.yuhei@gmail.com>
2018-06-10 19:53:45 +0900

    Concat application with other tables

--- join.py
+++ join.py
@@ -65,12 +65,42 @@ def concat_df(in_path, prefix):
     agg.to_feather(out_path)
 
 
+def concat_app():
+    def handle_category(df):
+        categorical_columns = df.select_dtypes('object').columns.tolist()
+        for c in categorical_columns:
+            df[c] = df[c].astype(
+                'category').cat.codes.apply(lambda d: 'c{}'.format(d))
+        return df
+
+    train_df = pd.read_feather('./data/application_train.csv.feather')
+    test_df = pd.read_feather('./data/application_test.csv.feather')
+    df = train_df.append(test_df).reset_index(drop=True)
+    df = handle_category(df)
+    for in_path in [
+        './data/bureau.concat.feather',
+        './data/credit_card_balance.concat.feather',
+        './data/POS_CASH_balance.concat.feather',
+        './data/installments_payments.concat.feather',
+        './data/previous_application.concat.feather',
+    ]:
+        print('load {}...'.format(in_path))
+        other_df = pd.read_feather(in_path, nthreads=12)
+        df = df.merge(other_df, on='SK_ID_CURR', how='left')
+    train_df, test_df = df[
+        :len(train_df)].reset_index(), df[len(train_df):].reset_index()
+    train_df['TARGET'] = train_df['TARGET'].astype('i')
+    train_df.to_feather('./data/application_train.concat.feather')
+    test_df.to_feather('./data/application_test.concat.feather')
+
+
 def main():
     concat_bureau()
     concat_df('./data/credit_card_balance.csv.feather', 'CREDIT')
     concat_df('./data/POS_CASH_balance.csv.feather', 'POS')
     concat_df('./data/installments_payments.csv.feather', 'INS')
     concat_df('./data/previous_application.csv.feather', 'PREV')
+    concat_app()
 
 
 if __name__ == '__main__':

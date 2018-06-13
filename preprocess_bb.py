8276c4a9017c05d901f8f736ab2070aef29baa20
nel215 <otomo.yuhei@gmail.com>
2018-06-13 21:30:34 +0900

    Separate preprocessing of bb

---
+++ preprocess_bb.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+def preprocess_bb():
+    bb_df = pd.read_feather('./data/bureau_balance.feather')
+    bb_df['GOOD_STATUS'] = bb_df[
+        'STATUS'].apply(lambda d: d in ['C', '0']).astype('i')
+    bb_df['BAD_STATUS'] = bb_df[
+        'STATUS'].apply(lambda d: d not in ['C', '0', 'X']).astype('i')
+    bb_df = pd.get_dummies(bb_df, columns=['STATUS'])
+    return bb_df
+
+
+def main():
+    bb_df = preprocess_bb()
+    bb_df.to_feather('./data/bureau_balance.preprocessed.feather')
+
+
+if __name__ == '__main__':
+    main()

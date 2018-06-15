ab2cf2737ed249c9d3d9933f6ef9995795b4a3db
nel215 <otomo.yuhei@gmail.com>
2018-06-15 23:44:31 +0900

    Fix script to aggregate prev

--- aggregate_prev.py
+++ aggregate_prev.py
@@ -3,85 +3,86 @@ from utility import one_hot_encoder
 pd.set_option("display.max_columns", 100)
 
 
-def create_new_features(prev_df):
-    grp = prev_df.groupby('SK_ID_CURR')
-    grp = grp[['SK_ID_PREV']].count()
-    grp.columns = ['PREV_COUNT']
-    res = grp.reset_index()
+def preprocess_prev():
+    pre_df = pd.read_feather('./data/previous_application.joined.feather')
+
+    # TODO: Move preprocess prev
+    pre_df['IS_REFUSED'] =\
+        (pre_df['NAME_CONTRACT_STATUS'] == 'Refused').astype('i')
+
+    pre_df = pre_df.sort_values(['SK_ID_CURR', 'SK_ID_PREV'])
+    pre_df, cat_columns = one_hot_encoder(pre_df)
+
+    grp = pre_df.groupby('SK_ID_CURR')
+    g = grp[['SK_ID_PREV']].count()
+    g.columns = ['COUNT']
+    pre_agg = g
 
     # REFUSED RATIO
-    grp = prev_df
-    grp['IS_REFUSED'] = (grp['NAME_CONTRACT_STATUS'] == 'Refused').astype('i')
-    grp = grp.groupby('SK_ID_CURR')
-    grp = grp[['IS_REFUSED']].sum()
-    res = res.merge(grp, on='SK_ID_CURR', how='left')
-    res['REFUSED_ratio'] = res['IS_REFUSED'] / res['PREV_COUNT']
+    g = grp[['IS_REFUSED']].sum()
+    pre_agg = pre_agg.join(g, on='SK_ID_CURR', how='left')
+    pre_agg['RATIO_REFUSED'] = pre_agg['IS_REFUSED'] / pre_agg['COUNT']
 
     # NOT COMPLETE RATIO
-    grp = prev_df[
-        prev_df['DAYS_LAST_DUE_1ST_VERSION'] < 365243.0].reset_index()
-    grp['NOT_COMPLETE'] = (grp['DAYS_LAST_DUE_1ST_VERSION'] >= 0).astype('i')
-    grp = grp.groupby('SK_ID_CURR')
-    grp = grp[['NOT_COMPLETE']].sum()
-    res = res.merge(grp, on='SK_ID_CURR', how='left')
-    res['NOT_COMPLETE_ratio'] = res['NOT_COMPLETE'] / res['PREV_COUNT']
-
-    return res
-
-
-def preprocess_prev(prev_df):
-    prev_df, cat_cols = one_hot_encoder(prev_df)
+    g = pre_df[
+        pre_df['DAYS_LAST_DUE_1ST_VERSION'] < 365243.0].reset_index()
+    g['NOT_COMPLETE'] = (g['DAYS_LAST_DUE_1ST_VERSION'] >= 0).astype('i')
+    g = g.groupby('SK_ID_CURR')
+    g = g[['NOT_COMPLETE']].sum()
+    pre_agg = pre_agg.join(g, on='SK_ID_CURR', how='left')
+    pre_agg['RATIO_NOT_COMPLETE'] = pre_agg['NOT_COMPLETE'] / pre_agg['COUNT']
 
     # Previous applications numeric features
-    num_aggregations = {
-        'AMT_ANNUITY': ['min', 'max', 'mean'],
-        'AMT_APPLICATION': ['min', 'max', 'mean'],
-        'AMT_CREDIT': ['min', 'max', 'mean'],
-        'APP_CREDIT_PERC': ['min', 'max', 'mean', 'var'],
-        'AMT_DOWN_PAYMENT': ['min', 'max', 'mean'],
-        'AMT_GOODS_PRICE': ['min', 'max', 'mean'],
-        'HOUR_APPR_PROCESS_START': ['min', 'max', 'mean'],
-        'RATE_DOWN_PAYMENT': ['min', 'max', 'mean'],
-        'DAYS_DECISION': ['min', 'max', 'mean'],
-        'CNT_PAYMENT': ['mean', 'sum'],
+    fs = ['sum', 'median', 'mean', 'std', 'max', 'min']
+    agg = {
+        'AMT_ANNUITY': fs,
+        'AMT_APPLICATION': fs,
+        'AMT_CREDIT': fs,
+        'RATIO_APP_TO_CREDIT': fs,
+        'AMT_DOWN_PAYMENT': fs,
+        'AMT_GOODS_PRICE': fs,
+        'HOUR_APPR_PROCESS_START': fs,
+        'RATE_DOWN_PAYMENT': fs,
+        'DAYS_DECISION': fs,
+        'CNT_PAYMENT': fs,
     }
-    # Previous applications categorical features
-    cat_aggregations = {}
-    for cat in cat_cols:
-        cat_aggregations[cat] = ['mean']
+    for c in cat_columns:
+        agg[c] = ['mean']
 
-    prev_agg = prev_df.groupby(
-        'SK_ID_CURR').agg({**num_aggregations, **cat_aggregations})
-    prev_agg.columns = [
-        'PREV_' + e[0] + "_" + e[1].upper() for e in prev_agg.columns]
-    # Previous Applications: Approved Applications - only numerical features
-    approved = prev_df[prev_df['NAME_CONTRACT_STATUS_Approved'] == 1]
-    approved_agg = approved.groupby('SK_ID_CURR').agg(num_aggregations)
+    for c in pre_df.columns:
+        if c == 'SK_ID_CURR' or c == 'SK_ID_PREV':
+            continue
+        if c in agg:
+            continue
+        agg[c] = ['mean']
+
+    g = grp.agg(agg)
+    g.columns = ['{}_{}'.format(a.upper(), b) for a, b in g.columns]
+    pre_agg = pre_agg.join(g, on='SK_ID_CURR', how='left')
+
+    # Previous Applications: Approved Applications
+    approved = pre_df[pre_df['NAME_CONTRACT_STATUS_Approved'] == 1]
+    approved_agg = approved.groupby('SK_ID_CURR').agg(agg)
     approved_agg.columns = [
-        'APPROVED_' + e[0] + "_" + e[1].upper() for e in approved_agg.columns]
-    prev_agg = prev_agg.join(approved_agg, how='left', on='SK_ID_CURR')
-    # Previous Applications: Refused Applications - only numerical features
-    refused = prev_df[prev_df['NAME_CONTRACT_STATUS_Refused'] == 1]
-    refused_agg = refused.groupby('SK_ID_CURR').agg(num_aggregations)
-    refused_agg.columns = [
-        'REFUSED_' + e[0] + "_" + e[1].upper() for e in refused_agg.columns]
-    prev_agg = prev_agg.join(refused_agg, how='left', on='SK_ID_CURR')
+        'APPROVED_{}_{}'.format(a.upper(), b) for a, b in approved_agg.columns]
 
-    return prev_agg
+    pre_agg = pre_agg.join(approved_agg, on='SK_ID_CURR', how='left')
+
+    # Previous Applications: Refused Applications
+    refused = pre_df[pre_df['NAME_CONTRACT_STATUS_Refused'] == 1]
+    refused_agg = refused.groupby('SK_ID_CURR').agg(agg)
+    refused_agg.columns = [
+        'REFUSED_{}_{}'.format(a.upper(), b) for a, b in refused_agg.columns]
+    pre_agg = pre_agg.join(refused_agg, on='SK_ID_CURR', how='left')
+
+    pre_agg.columns = ['PREV_{}'.format(c) for c in pre_agg.columns]
+
+    return pre_agg.reset_index()
 
 
 def main():
-    prev_df = pd.read_feather(
-        './data/previous_application.preprocessed.feather')
-    prev_df = prev_df.sort_values('SK_ID_CURR')
-    prev_agg = preprocess_prev(prev_df)
-    res = create_new_features(prev_df)
-    res = res.merge(prev_agg, how='left', on='SK_ID_CURR')
-    res = res.set_index('SK_ID_CURR')
-    res.columns = [
-        'PREV_{}'.format(c.replace(' ', '_')) for c in res.columns]
-    res = res.reset_index()
-    res.to_feather('./data/previous_application.agg.feather')
+    pre_agg = preprocess_prev()
+    pre_agg.to_feather('./data/previous_application.agg.feather')
 
 
 if __name__ == '__main__':

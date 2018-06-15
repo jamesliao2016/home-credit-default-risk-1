5637420d9864f860e71f65000a4705c2a8442008
nel215 <otomo.yuhei@gmail.com>
2018-06-15 21:26:14 +0900

    Add TODO

--- aggregate_credit.py
+++ aggregate_credit.py
@@ -2,6 +2,7 @@ import pandas as pd
 pd.set_option("display.max_columns", 100)
 pd.set_option("display.max_rows", 200)
 pd.set_option("display.width", 180)
+# TODO: Add last status
 
 
 def handle_numeric(df, res, target):

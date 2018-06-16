987f11171006d12de7c749fc53838ad692192b6f
nel215 <otomo.yuhei@gmail.com>
2018-06-16 23:28:24 +0900

    Refactor splitter

--- split_train.py
+++ split_train.py
@@ -2,8 +2,9 @@ import numpy as np
 import pandas as pd
 
 
-def split_preprocessed():
-    df = pd.read_feather('./data/application_train.preprocessed.feather')
+def _split(src, dst_format):
+    np.random.seed(215)
+    df = pd.read_feather(src)
     pos_df = df[df['TARGET'] == 1].reset_index(drop=True)
     neg_df = df[df['TARGET'] == 0].sample(frac=1).reset_index(drop=True)
     del df
@@ -12,28 +13,26 @@ def split_preprocessed():
         e = min(s+chunck_size, len(neg_df))
         df = pd.concat(
             [pos_df, neg_df[s:e]]).sample(frac=1).reset_index(drop=True)
-        dst = './data/application_train.preprocessed.split.{}.feather'.format(i)  # noqa
+        dst = dst_format.format(i)  # noqa
         df.to_feather(dst)
 
 
 def split():
-    df = pd.read_feather('./data/application_train.feather')
-    pos_df = df[df['TARGET'] == 1].reset_index(drop=True)
-    neg_df = df[df['TARGET'] == 0].sample(frac=1).reset_index(drop=True)
-    del df
-    chunck_size = len(neg_df) // 11 + 1
-    for i, s in enumerate(range(0, len(neg_df), chunck_size)):
-        e = min(s+chunck_size, len(neg_df))
-        df = pd.concat(
-            [pos_df, neg_df[s:e]]).sample(frac=1).reset_index(drop=True)
-        dst = './data/application_train.split.{}.feather'.format(i)
-        df.to_feather(dst)
+    _split(
+        './data/application_train.feather',
+        './data/application_train.split.{}.feather',
+    )
+
+
+def split_preprocessed():
+    _split(
+        './data/application_train.preprocessed.feather',
+        './data/application_train.preprocessed.split.{}.feather',
+    )
 
 
 def main():
-    np.random.seed(215)
     split()
-    np.random.seed(215)
     split_preprocessed()
 
 

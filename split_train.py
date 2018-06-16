d5b821b2b7c469e1ae733d687564c417eb0b4322
nel215 <otomo.yuhei@gmail.com>
2018-06-16 22:02:11 +0900

    Fix splitter

--- split_train.py
+++ split_train.py
@@ -2,8 +2,22 @@ import numpy as np
 import pandas as pd
 
 
-def split():
+def split_preprocessed():
     df = pd.read_feather('./data/application_train.preprocessed.feather')
+    pos_df = df[df['TARGET'] == 1].reset_index(drop=True)
+    neg_df = df[df['TARGET'] == 0].sample(frac=1).reset_index(drop=True)
+    del df
+    chunck_size = len(neg_df) // 11 + 1
+    for i, s in enumerate(range(0, len(neg_df), chunck_size)):
+        e = min(s+chunck_size, len(neg_df))
+        df = pd.concat(
+            [pos_df, neg_df[s:e]]).sample(frac=1).reset_index(drop=True)
+        dst = './data/application_train.preprocessed.split.{}.feather'.format(i)  # noqa
+        df.to_feather(dst)
+
+
+def split():
+    df = pd.read_feather('./data/application_train.feather')
     pos_df = df[df['TARGET'] == 1].reset_index(drop=True)
     neg_df = df[df['TARGET'] == 0].sample(frac=1).reset_index(drop=True)
     del df
@@ -19,6 +33,8 @@ def split():
 def main():
     np.random.seed(215)
     split()
+    np.random.seed(215)
+    split_preprocessed()
 
 
 if __name__ == '__main__':

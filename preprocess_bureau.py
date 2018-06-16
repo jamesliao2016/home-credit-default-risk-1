ecb79d68c0b7c3eae7c8910feb238f4232ef7797
nel215 <otomo.yuhei@gmail.com>
2018-06-16 17:12:35 +0900

    Refactor preprocess of bureau

--- preprocess_bureau.py
+++ preprocess_bureau.py
@@ -1,166 +1,56 @@
 import pandas as pd
-from utility import one_hot_encoder
 pd.set_option("display.max_columns", 100)
+pd.set_option("display.width", 200)
 
 
-def handle_category(bure_df, res, target):
-    grp = bure_df.groupby('SK_ID_CURR')
-    grp = grp[[target]].nunique()
-    grp.columns = ['{}_nunique'.format(target)]
-    res = res.merge(grp.reset_index(), on='SK_ID_CURR', how='left')
-    res['{}_diversity'.format(target)] = res[
-        '{}_nunique'.format(target)] / res['BUREAU_COUNT']
-
-    categories = bure_df[target].unique().tolist()
-    for c in categories:
-        grp = bure_df[bure_df[target] == c].groupby('SK_ID_CURR')
-        grp = grp[[target]].count()
-        col = '{}_{}_ratio'.format(target, c).replace(' ', '_')
-        grp.columns = [col]
-        res = res.merge(grp.reset_index(), on='SK_ID_CURR', how='left')
-        res[col].fillna(0, inplace=True)
-        res[col] /= res['BUREAU_COUNT']
-
-    return res
+def add_diff(df):
+    ts_features = [
+        'DAYS_CREDIT',
+        'DAYS_CREDIT_ENDDATE',
+        'DAYS_ENDDATE_FACT',
+        'DAYS_CREDIT_UPDATE',
+    ]
+    grp = df.groupby('SK_ID_CURR')
+    diff = grp.diff()[ts_features]
+    diff.columns = ['TSDIFF_{}'.format(c) for c in diff.columns]
+    for c in diff.columns:
+        df[c] = diff[c]
 
 
-def handle_numeric(bure_df, res, target):
-    grp = bure_df.groupby('SK_ID_CURR')
-    grp = grp[[target]].agg(
-        ['sum', 'mean', 'std', 'min', 'max'])
-    grp.columns = ['{}_{}'.format(
-        target, c) for c in ['sum', 'mean', 'std', 'min', 'max']]
-    res = res.merge(grp.reset_index(), on='SK_ID_CURR', how='left')
-    return res
+def preprocess_bureau():
+    df = join_bb()
+    df = df.sort_values(
+        ['SK_ID_CURR', 'DAYS_CREDIT'],
+    ).reset_index(drop=True)
+    df = df[:10000]
+    df['DAYS_CREDIT_ENDDATE_PLUS'] = (
+        df['DAYS_CREDIT_ENDDATE'] >= 0).astype('i')
+    df['AMT_CREDIT_SUM'].fillna(0, inplace=True)
+    df['AMT_CREDIT_SUM_DEBT'].fillna(0, inplace=True)
+    df['AMT_CREDIT_SUM_OVERDUE'].fillna(0, inplace=True)
+    df['AMT_CREDIT_SUM_LIMIT'].fillna(0, inplace=True)
+    df['CNT_CREDIT_PROLONG'].fillna(0, inplace=True)
+    df['AMT_ANNUITY'].fillna(0, inplace=True)
+    df['BB_COUNT'].fillna(0, inplace=True)
+    df['BB_STATUS_NUNIQUE'].fillna(0, inplace=True)
+    df['BB_GOOD_STATUS_MEAN'].fillna(1, inplace=True)
+    df['BB_BAD_STATUS_MEAN'].fillna(0, inplace=True)
+    df['BB_TERM'].fillna(0, inplace=True)
+    add_diff(df)
+    return df
 
 
-def create_new_features(bure_df):
-    grp = bure_df.groupby('SK_ID_CURR')
-    grp = grp[['SK_ID_BUREAU']].count()
-    grp.columns = ['BUREAU_COUNT']
-    res = grp.reset_index()
-
-    tmp = bure_df[['SK_ID_CURR', 'DAYS_CREDIT']]
-    tmp = tmp.sort_values(['SK_ID_CURR', 'DAYS_CREDIT'], ascending=False)
-    grp = tmp.groupby('SK_ID_CURR').shift(1)
-    tmp['DAYS_CREDIT_diff'] = tmp['DAYS_CREDIT'] - grp['DAYS_CREDIT']
-    null_idx = tmp[pd.isnull(tmp['DAYS_CREDIT_diff'])].index
-    tmp.loc[null_idx, 'DAYS_CREDIT_diff'] = tmp.loc[null_idx, 'DAYS_CREDIT']
-    grp = tmp.groupby('SK_ID_CURR')[['DAYS_CREDIT_diff']].agg(
-        ['mean', 'std', 'min', 'max'])
-    grp.columns = ['DAYS_CREDIT_DIFF_{}'.format(
-        c) for c in ['mean', 'std', 'min', 'max']]
-    res = res.merge(grp.reset_index(), on='SK_ID_CURR', how='left')
-
-    bure_df['DAYS_CREDIT_ENDDATE_PLUS'] = (
-        bure_df['DAYS_CREDIT_ENDDATE'] >= 0).astype('i')
-    grp = bure_df.groupby('SK_ID_CURR')
-    grp = grp[['DAYS_CREDIT_ENDDATE_PLUS']].sum()
-    grp.columns = ['DAYS_CREDIT_ENDDATE_PLUS_count']
-    res = res.merge(grp.reset_index(), on='SK_ID_CURR', how='left')
-    res['DAYS_CREDIT_ENDDATE_PLUS_ratio'] = res[
-        'DAYS_CREDIT_ENDDATE_PLUS_count'] / res['BUREAU_COUNT']
-
-    tmp = bure_df[bure_df['DAYS_CREDIT_ENDDATE_PLUS'] > 0]
-    tmp = tmp[['SK_ID_CURR', 'DAYS_CREDIT_ENDDATE']]
-    tmp = tmp.sort_values(['SK_ID_CURR', 'DAYS_CREDIT_ENDDATE'])
-    grp = tmp.groupby('SK_ID_CURR').shift(1)
-    tmp['DAYS_CREDIT_ENDDATE_diff'] = tmp[
-        'DAYS_CREDIT_ENDDATE'] - grp['DAYS_CREDIT_ENDDATE']
-    null_idx = tmp[pd.isnull(tmp['DAYS_CREDIT_ENDDATE_diff'])].index
-    tmp.loc[null_idx, 'DAYS_CREDIT_ENDDATE_diff'] = tmp.loc[
-        null_idx, 'DAYS_CREDIT_ENDDATE']
-    grp = tmp.groupby('SK_ID_CURR')[['DAYS_CREDIT_ENDDATE_diff']].agg(
-        ['mean', 'std', 'min', 'max'])
-    grp.columns = ['DAYS_CREDIT_ENDDATE_DIFF_{}'.format(
-        c) for c in ['mean', 'std', 'min', 'max']]
-    res = res.merge(grp.reset_index(), on='SK_ID_CURR', how='left')
-
-    bure_df['AMT_CREDIT_SUM'].fillna(0, inplace=True)
-    bure_df['AMT_CREDIT_SUM_DEBT'].fillna(0, inplace=True)
-    bure_df['AMT_CREDIT_SUM_OVERDUE'].fillna(0, inplace=True)
-    bure_df['CNT_CREDIT_PROLONG'].fillna(0, inplace=True)
-    res = handle_numeric(bure_df, res, 'AMT_CREDIT_SUM')
-    res = handle_numeric(bure_df, res, 'AMT_CREDIT_SUM_DEBT')
-    res = handle_numeric(bure_df, res, 'AMT_CREDIT_SUM_OVERDUE')
-    res = handle_numeric(bure_df, res, 'CNT_CREDIT_PROLONG')
-    res['CREDIT_DEBT_ratio'] = res[
-        'AMT_CREDIT_SUM_DEBT_sum'] / res['AMT_CREDIT_SUM_sum']
-    res['CREDIT_OVERDUE_ratio'] = res[
-        'AMT_CREDIT_SUM_OVERDUE_sum'] / res['AMT_CREDIT_SUM_DEBT_sum']
-
-    res = handle_category(bure_df, res, 'CREDIT_CURRENCY')
-    res = handle_category(bure_df, res, 'CREDIT_ACTIVE')
-    res = handle_category(bure_df, res, 'CREDIT_TYPE')
-
-    return res
-
-
-def preprocess_bureau_and_balance(bure_df, bb_agg):
-    bure_df, bureau_cat = one_hot_encoder(bure_df)
-
-    bure_df = bure_df.merge(bb_agg, how='left', on='SK_ID_BUREAU')
-    bure_df.drop(columns='SK_ID_BUREAU', inplace=True)
-
-    # Bureau and bureau_balance numeric features
-    num_aggregations = {
-        'DAYS_CREDIT': ['min', 'max', 'mean', 'var'],
-        'CREDIT_DAY_OVERDUE': ['max', 'mean'],
-        'DAYS_CREDIT_ENDDATE': ['min', 'max', 'mean'],
-        'AMT_CREDIT_MAX_OVERDUE': ['mean'],
-        'CNT_CREDIT_PROLONG': ['sum'],
-        'AMT_CREDIT_SUM': ['max', 'mean', 'sum'],
-        'AMT_CREDIT_SUM_DEBT': ['max', 'mean', 'sum'],
-        'AMT_CREDIT_SUM_OVERDUE': ['mean'],
-        'AMT_CREDIT_SUM_LIMIT': ['mean', 'sum'],
-        'DAYS_CREDIT_UPDATE': ['min', 'max', 'mean'],
-        'AMT_ANNUITY': ['max', 'mean'],
-        'BB_MONTHS_BALANCE_MIN': ['min'],
-        'BB_MONTHS_BALANCE_MAX': ['max'],
-        'BB_MONTHS_BALANCE_COUNT': ['mean', 'sum'],
-        'BB_TERM': ['min', 'max', 'mean', 'sum'],
-    }
-    for c in bb_agg.columns:
-        if c == 'SK_ID_BUREAU' or c in num_aggregations:
-            continue
-        num_aggregations[c] = ['mean']
-
-    # Bureau and bureau_balance categorical features
-    cat_aggregations = {}
-    for cat in bureau_cat:
-        cat_aggregations[cat] = ['mean']
-
-    bureau_agg = bure_df.groupby(
-        'SK_ID_CURR').agg({**num_aggregations, **cat_aggregations})
-    bureau_agg.columns = [
-        'BURE_' + a + "_" + b.upper() for a, b in bureau_agg.columns]
-    # Bureau: Active credits - using only numerical aggregations
-    active = bure_df[bure_df['CREDIT_ACTIVE_Active'] == 1]
-    active_agg = active.groupby('SK_ID_CURR').agg(num_aggregations)
-    active_agg.columns = [
-        'ACTIVE_' + a + "_" + b.upper() for a, b in active_agg.columns]
-    bureau_agg = bureau_agg.join(active_agg, how='left', on='SK_ID_CURR')
-
-    # Bureau: Closed credits - using only numerical aggregations
-    closed = bure_df[bure_df['CREDIT_ACTIVE_Closed'] == 1]
-    closed_agg = closed.groupby('SK_ID_CURR').agg(num_aggregations)
-    closed_agg.columns = [
-        'CLOSED_' + a + "_" + b.upper() for a, b in closed_agg.columns]
-    bureau_agg = bureau_agg.join(closed_agg, how='left', on='SK_ID_CURR')
-    return bureau_agg
+def join_bb():
+    df = pd.read_feather('./data/bureau.feather')
+    agg = pd.read_feather('./data/bureau_balance.agg.feather')
+    df = df.merge(agg, on='SK_ID_BUREAU', how='left')
+    del df['SK_ID_BUREAU']
+    return df
 
 
 def main():
-    bure_df = pd.read_feather('./data/bureau.feather')
-    bb_agg = pd.read_feather('./data/bureau_balance.agg.feather')
-    bure_agg = preprocess_bureau_and_balance(bure_df, bb_agg)
-    res = create_new_features(bure_df)
-    res = res.merge(bure_agg, how='left', on='SK_ID_CURR')
-    res = res.set_index('SK_ID_CURR')
-    res.columns = [
-        'BURE_{}'.format(c) for c in res.columns]
-    res = res.reset_index()
-    res.to_feather('data/bureau.agg.feather')
+    bure_df = preprocess_bureau()
+    bure_df.to_feather('data/bureau.preprocessed.feather')
 
 
 if __name__ == '__main__':

844b9022e7b1bab730f204746fb4336f2ec00ad4
nel215 <otomo.yuhei@gmail.com>
2018-06-16 13:09:37 +0900

    Fix aggregate of inst

--- aggregate_inst.py
+++ aggregate_inst.py
@@ -1,34 +1,43 @@
 import pandas as pd
-from utility import one_hot_encoder
 pd.set_option("display.max_columns", 100)
 pd.set_option("display.width", 180)
 
 
 def aggregate_inst():
     df = pd.read_feather('./data/installments_payments.preprocessed.feather')
-    df, cat_columns = one_hot_encoder(df)
+    df = df.sort_values(['SK_ID_CURR', 'SK_ID_PREV', 'DAYS_INSTALMENT'])
+
+    # TODO: Move to preprocess
+    df['IS_CREDIT'] = (df['NUM_INSTALMENT_VERSION'] == 0).astype('i')
 
     key = ['SK_ID_CURR', 'SK_ID_PREV']
     # Features: Perform aggregations
     agg = {
+        # original
         'NUM_INSTALMENT_VERSION': ['nunique'],
+        'NUM_INSTALMENT_NUMBER': ['max'],
+        'AMT_INSTALMENT': ['min', 'max', 'mean', 'sum'],
+        'AMT_PAYMENT': ['min', 'max', 'mean', 'sum', 'std'],
+        'DAYS_ENTRY_PAYMENT': ['max', 'min'],
+        # preprocessed
+        'RATIO_PAYMENT': ['max', 'min', 'mean', 'std'],
+        'DIFF_PAYMENT': ['max', 'min', 'mean', 'sum', 'std'],
         'DPD': ['max', 'mean', 'sum'],
         'DBD': ['max', 'mean', 'sum'],
-        'PAYMENT_PERC': ['max', 'mean', 'sum', 'var'],
-        'PAYMENT_DIFF': ['max', 'mean', 'sum', 'var'],
-        'AMT_INSTALMENT': ['max', 'mean', 'sum'],
-        'AMT_PAYMENT': ['min', 'max', 'mean', 'sum'],
-        'DAYS_ENTRY_PAYMENT': ['max', 'mean', 'sum']
+        'IS_CREDIT': ['mean'],
+        'TSDIFF_DAYS_INSTALMENT': ['mean', 'std'],
+        'TSDIFF_DAYS_ENTRY_PAYMENT': ['mean', 'std'],
+        'TSDIFF_AMT_INSTALMENT': ['mean', 'std'],
+        'TSDIFF_AMT_PAYMENT': ['mean', 'std'],
     }
-    for cat in cat_columns:
-        agg[cat] = ['mean']
+
     grp = df.groupby(key)
     g = grp.agg(agg)
     g.columns = [a + "_" + b.upper() for a, b in g.columns]
     ins_agg = g
 
     # Count installments accounts
-    ins_agg['INS_COUNT'] = grp.size()
+    ins_agg['COUNT'] = grp.size()
     ins_agg.columns = ["INS_" + c for c in ins_agg.columns]
     return ins_agg.reset_index()
 

9d321d0d478fc33ba31a9253662ce8fdcd68113b
nel215 <otomo.yuhei@gmail.com>
2018-06-17 00:02:58 +0900

    Add features

--- preprocess_application.py
+++ preprocess_application.py
@@ -23,6 +23,14 @@ def preprocess_application():
     df['WORKING_LIFE_RATIO'] = df['DAYS_EMPLOYED'] / df['DAYS_BIRTH']
     df['INCOME_PER_FAM'] = df['AMT_INCOME_TOTAL'] / df['CNT_FAM_MEMBERS']
     df['CHILDREN_RATIO'] = df['CNT_CHILDREN'] / df['CNT_FAM_MEMBERS']
+
+    # from https://www.kaggle.com/shep312/lightgbm-harder-better-slower
+    df['CONSUMER_GOODS_RATIO'] = df['AMT_CREDIT'] / df['AMT_GOODS_PRICE']
+    df['ANN_LENGTH_EMPLOYED_RATIO'] =\
+        df['ANNUITY_LENGTH'] / df['DAYS_EMPLOYED']
+    df['TOTAL_DOCS_SUBMITTED'] =\
+        df.loc[:, df.columns.str.contains('FLAG_DOCUMENT')].sum(axis=1)
+
     df.columns = [c.replace(' ', '_') for c in df.columns]
 
     train_df = df[:n_train].reset_index(drop=True)

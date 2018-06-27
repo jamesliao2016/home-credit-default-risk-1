71d8a01c5c6d5588ebeb2bc2c3f4b2b403b1399a
nel215 <otomo.yuhei@gmail.com>
2018-06-28 08:52:34 +0900

    Add features from inst

--- aggregate_inst.py
+++ aggregate_inst.py
@@ -6,7 +6,8 @@ def aggregate_inst(df, key):
         'NUM_INSTALMENT_NUMBER': ['max'],
         'AMT_INSTALMENT': ['min', 'max', 'mean', 'sum'],
         'AMT_PAYMENT': ['min', 'max', 'mean', 'sum', 'std'],
-        'DAYS_ENTRY_PAYMENT': ['max', 'min'],
+        'DAYS_ENTRY_PAYMENT': ['sum', 'max', 'min'],
+        'DAYS_INSTALMENT': ['sum', 'max', 'min'],
         # preprocessed
         'RATIO_PAYMENT': ['max', 'min', 'mean', 'std'],
         'DIFF_PAYMENT': ['max', 'min', 'mean', 'sum', 'std'],
@@ -26,5 +27,8 @@ def aggregate_inst(df, key):
     agg = g
 
     agg['COUNT'] = grp.size()
+
+    agg['AMT_DIFF_PAYMENT_INSTALMENT'] = agg['AMT_INSTALMENT_SUM'] - agg['AMT_PAYMENT_SUM']
+    agg['DAYS_DIFF_PAYMENT_INSTALMENT'] = agg['DAYS_INSTALMENT_SUM'] - agg['DAYS_ENTRY_PAYMENT_SUM']
 
     return agg

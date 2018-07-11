99020aa29f38fafd217186d48767092e55ab2e9c
nel215 <otomo.yuhei@gmail.com>
2018-07-11 22:19:08 +0900

    Report auc

--- train_tempen.py
+++ train_tempen.py
@@ -5,13 +5,15 @@ import numpy as np
 import pandas as pd
 import chainer.links as L
 import chainer.functions as F
-from chainer import Chain
+from chainer import Chain, cuda
 from chainer.optimizers import Adam
 from chainer.training import Trainer
 from chainer.training import extensions, make_extension
 from chainer.iterators import SerialIterator
 from chainer.training.updaters import StandardUpdater
+from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
+from utility import reset_seed
 
 
 class UpdateExtention():
@@ -29,7 +31,8 @@ class TemporalEmsenbling(Chain):
         super(TemporalEmsenbling, self).__init__()
         with self.init_scope():
             self.l1 = L.Linear(None, 128)
-            self.l2 = L.Linear(None, 1)
+            self.l2 = L.Linear(None, 64)
+            self.l3 = L.Linear(None, 1)
 
         self.n = n
         self.initialized = False
@@ -47,6 +50,17 @@ class TemporalEmsenbling(Chain):
         self.z_hat = self.Z / (1.-xp.power(alpha, t))
         self.z = xp.zeros((self.n, 1))
 
+    def forward(self, x):
+        # TODO: augmentation
+        h = self.l1(x)
+        h = F.relu(h)
+        h = F.dropout(h)
+        h = self.l2(h)
+        h = F.relu(h)
+        h = F.dropout(h)
+        h = self.l3(h)
+        return h
+
     def __call__(self, data):
         if not self.initialized:
             self.reset()
@@ -54,28 +68,30 @@ class TemporalEmsenbling(Chain):
         x = data[:, :-2]  # features
         i = data[:, -2].astype('i')  # index
         y = data[:, -1:]  # label
+
+        h = self.forward(x)
+
+        self.z[i] = h.array
+
         xp = self.xp
-        # TODO: augmentation
-        h = self.l1(x)
-        h = F.relu(h)
-        h = F.dropout(h)
-        h = self.l2(h)
-
-        z = F.sigmoid(h)
-        self.z[i] = z.array
-
         idx = ~xp.isnan(y)
 
         loss = 0
         loss += F.bernoulli_nll(y[idx], h[idx]) / len(y[idx])
-        loss += F.mean_squared_error(z, self.z_hat[i])
+        loss += F.mean_squared_error(h, self.z_hat[i])
+
+        a = cuda.to_cpu(y[idx]).reshape(-1).astype('i')
+        b = cuda.to_cpu(F.sigmoid(h)[idx].array).reshape(-1)
+        score = roc_auc_score(a, b)
 
         chainer.report({'loss': loss}, self)
+        chainer.report({'score': score}, self)
 
         return loss
 
 
 def main():
+    reset_seed(0)
     train = pd.read_feather('./data/application_train.preprocessed.feather')
     test = pd.read_feather('./data/application_test.preprocessed.feather')
     test['TARGET'] = np.nan
@@ -96,13 +112,13 @@ def main():
     test_dset = test[features].values.astype('f')
 
     train_dset, valid_dset = train_test_split(
-        train_dset, test_size=0.1, random_state=215,
+        train_dset, test_size=0.20, random_state=215,
         stratify=train['TARGET'])
 
     train_dset = np.vstack([train_dset, test_dset])
     print(train_dset.shape)
 
-    train_iter = SerialIterator(train_dset, 256)
+    train_iter = SerialIterator(train_dset, 512)
     valid_iter = SerialIterator(valid_dset, len(valid_dset), False, False)
 
     model = TemporalEmsenbling(len(df))
@@ -118,8 +134,9 @@ def main():
     trainer.extend(extensions.Evaluator(
         valid_iter, model, device=0), name='val')
     trainer.extend(extensions.PrintReport([
-        'epoch', 'main/loss',
-        'val/main/loss', 'elapsed_time']))
+        'epoch', 'main/loss', 'main/score',
+        'val/main/loss', 'val/main/score',
+        'elapsed_time']))
     trainer.extend(extensions.PlotReport(
         ['main/loss', 'val/main/loss'],
         x_key='epoch', file_name='loss.png'))

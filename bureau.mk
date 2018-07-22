273ad2782de33bce491f7241f4ec8d617baa3ae1
nel215 <otomo.yuhei@gmail.com>
2018-07-22 12:01:00 +0900

    Create edge bb

--- bureau.mk
+++ bureau.mk
@@ -1,6 +1,9 @@
 data/bureau_balance.preprocessed.feather: data/bureau_balance.feather preprocess_bb.py
 	python preprocess_bb.py
 
+data/bb.edge.feather: data/bureau_balance.preprocessed.feather create_edge_bb.py
+	python create_edge_bb.py
+
 data/bureau_balance.agg.feather: data/bureau_balance.preprocessed.feather aggregate_bb.py
 	python aggregate_bb.py
 
@@ -9,4 +12,3 @@ data/bureau.preprocessed.feather: data/bureau_balance.agg.feather preprocess_bur
 
 data/bureau.agg.feather: data/bureau.preprocessed.feather aggregate_bureau.py
 	python aggregate_bureau.py
-
---
+++ create_edge_bb.py
@@ -0,0 +1,25 @@
+import pandas as pd
+pd.set_option("display.max_columns", 100)
+pd.set_option("display.width", 220)
+
+
+def main():
+    bb = pd.read_feather('./data/bureau_balance.preprocessed.feather')
+    bb = bb.sort_values(['SK_ID_BUREAU', 'MONTHS_BALANCE'])
+
+    grp = bb.groupby(['SK_ID_BUREAU'])
+    last = grp.last()
+    last.columns = ['LAST_{}'.format(c) for c in last.columns]
+    first = grp.first()
+    first.columns = ['FIRST_{}'.format(c) for c in first.columns]
+
+    edge = last.join(first, on='SK_ID_BUREAU')
+    edge['MONTH_BALANCE_DIFF'] = edge['LAST_MONTHS_BALANCE'] - edge['FIRST_MONTHS_BALANCE']
+    edge.columns = ['BB_{}'.format(c) for c in edge.columns]
+    edge = edge.reset_index()
+
+    edge.to_feather('./data/bb.edge.feather')
+
+
+if __name__ == '__main__':
+    main()

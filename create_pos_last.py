4a72da26f1b84f472074206694b7facb0628d50a
nel215 <otomo.yuhei@gmail.com>
2018-07-31 22:42:18 +0900

    Create last of pos

---
+++ create_pos_last.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from utility import reduce_memory
+
+
+def main():
+    df = pd.read_feather('./data/pos.preprocessed.feather')
+    df = df.sort_values(['SK_ID_CURR', 'MONTHS_BALANCE'])
+    df = df.groupby('SK_ID_CURR').last()
+    df.columns = ['POS_LAST_{}'.format(c) for c in df.columns]
+    df = df.reset_index()
+    reduce_memory(df)
+    df.to_feather('./data/pos.last.feather')
+
+
+if __name__ == '__main__':
+    main()

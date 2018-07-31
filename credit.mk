20431f7ce531018b409d8aaa1dce7fe194eb1c1c
nel215 <otomo.yuhei@gmail.com>
2018-07-31 23:54:08 +0900

    Delete unused scripts

--- credit.mk
+++ credit.mk
@@ -1,12 +1,6 @@
 data/credit.preprocessed.feather: data/credit_card_balance.feather preprocess_credit.py
 	python preprocess_credit.py
 
-data/credit_card_balance.agg.prev.feather: data/credit_card_balance.preprocessed.feather aggregate_credit_prev.py
-	python aggregate_credit_prev.py
-
-data/credit_card_balance.agg.prev.last.feather: data/credit_card_balance.preprocessed.feather aggregate_credit_prev_last.py
-	python aggregate_credit_prev_last.py
-
 data/credit.agg.feather: data/credit.preprocessed.feather aggregate_credit_curr.py
 	python aggregate_credit_curr.py
 

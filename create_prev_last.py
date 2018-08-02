895d4ebf10974694ef55e478304ccb5ddb4001ff
nel215 <otomo.yuhei@gmail.com>
2018-08-02 20:30:20 +0900

    Refactor last of prev

--- create_prev_last.py
+++ create_prev_last.py
@@ -4,14 +4,15 @@ pd.set_option("display.width", 180)
 
 
 def main():
-    prev = pd.read_feather('./data/previous_application.preprocessed.feather')
+    prev = pd.read_feather('./data/prev.preprocessed.feather')
     prev = prev.sort_values(['SK_ID_CURR', 'DAYS_DECISION'])
 
     last = prev.groupby(['SK_ID_CURR']).last()
-    last.columns = ['PREVLAST_{}'.format(c) for c in last.columns]
+    last = last.drop(['SK_ID_PREV'], axis=1)
+    last.columns = ['PREV_LAST_{}'.format(c) for c in last.columns]
     last = last.reset_index()
 
-    last.to_feather('./data/previous_application.last.feather')
+    last.to_feather('./data/prev.last.feather')
 
 
 if __name__ == '__main__':
--- prev.mk
+++ prev.mk
@@ -1,2 +1,4 @@
 data/prev.preprocessed.feather: data/previous_application.feather preprocess_prev.py
 	python preprocess_prev.py
+data/prev.last.feather: data/prev.preprocessed.feather create_prev_last.py
+	python create_prev_last.py

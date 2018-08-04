a2d786cbb1080a1f02ce27d0c7ceac15be26c9f8
nel215 <otomo.yuhei@gmail.com>
2018-08-04 10:57:50 +0900

    Drop id

--- create_pos_last.py
+++ create_pos_last.py
@@ -6,6 +6,7 @@ def main():
     df = pd.read_feather('./data/pos.preprocessed.feather')
     df = df.sort_values(['SK_ID_CURR', 'MONTHS_BALANCE'])
     df = df.groupby('SK_ID_CURR').last()
+    df = df.drop(['SK_ID_PREV'], axis=1)
     df.columns = ['POS_LAST_{}'.format(c) for c in df.columns]
     df = df.reset_index()
     reduce_memory(df)

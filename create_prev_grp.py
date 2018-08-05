68545d875b7be839c9c5235c025c10efa1abc855
nel215 <otomo.yuhei@gmail.com>
2018-08-05 17:56:28 +0900

    Create grp of prev

---
+++ create_prev_grp.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from utility import reduce_memory
+
+
+def _create(by, fs):
+    print('create {}...'.format(by))
+    df = pd.read_feather('./data/prev.preprocessed.feather')
+    agg = df.groupby(by).agg(fs)
+    agg.columns = ['GRP_{}_{}_{}'.format('_'.join(by), a, b.upper()) for a, b in agg.columns]
+    features = []
+    df = df.set_index(by)
+    df = df.join(agg, on=by, how='left')
+    df = df.reset_index(drop=True)
+    for c, ms in fs.items():
+        for m in ms:
+            ac = 'GRP_{}_{}_{}'.format('_'.join(by), c, m.upper())
+            dc = 'DIFF_{}'.format(ac)
+            adc = 'ABS_DIFF_{}'.format(ac)
+            df[dc] = df[c] - df[ac]
+            df[adc] = (df[c] - df[ac]).abs()
+            features += [dc, adc]
+    df = df[['SK_ID_CURR'] + features]
+    df = df.groupby('SK_ID_CURR').mean()
+    reduce_memory(df)
+    return df
+
+
+def main():
+    res = []
+
+    fs = {
+        'CNT_PAYMENT': ['mean'],
+        'DAYS_LAST_DUE_1ST_VERSION': ['mean'],
+    }
+
+    for by in [
+        ['NAME_CONTRACT_STATUS'],
+        ['CODE_REJECT_REASON'],
+        ['NAME_YIELD_GROUP'],
+        ['NAME_CONTRACT_STATUS', 'CODE_REJECT_REASON'],
+    ]:
+        res.append(_create(by, fs))
+
+    res = pd.concat(res, axis=1)
+    res.columns = ['PREV_{}'.format(c) for c in res.columns]
+    res = res.reset_index()
+
+    print(res.columns, res.columns.shape)
+    res.to_feather('./data/prev.grp.feather')
+
+
+if __name__ == '__main__':
+    main()
